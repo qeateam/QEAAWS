5a5ff8740b938cad5d4b38662f353c8e228b6a75
radhika-pradeep <radhika.pradeep@hotmail.com>
2017-06-19 17:41:08 +0530

    Add files via upload

---
+++ src/test/java/com/qea/aws/utils/retrieve_configPoperties.java
@@ -0,0 +1,19 @@
+import java.util.ResourceBundle;
+import java.net.URL;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.*; 
+
+
+public void retrieve_configProperties(){
+
+ResourceBundle rb=ResourceBundle.getBundle("proprties.config");
+var1 =rb.getobject("");
+var2 =rb.getobject("");
+var3 =rb.getobject("");
+var4 =rb.getobject("");
+var5 =rb.getobject("");
+
+
+}
